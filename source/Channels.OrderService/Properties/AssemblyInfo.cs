37d8ed2df8117852b2ef4915d4fc71b57b49bb6e
michaelnoonan <michaelnoonan@gmail.com>
2015-10-15 15:25:42 +1000

    Version with GitVersionTask and show on the Index.

--- source/Channels.OrderService/Properties/AssemblyInfo.cs
+++ source/Channels.OrderService/Properties/AssemblyInfo.cs
@@ -22,14 +22,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d7c97d81-0fcb-495e-8a64-efdfaafa33e1")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Versioned by GitVersionTask
